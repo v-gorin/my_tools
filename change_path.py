7e41fd99910e6fc9d3402193801132fab94d3fc3
vgorin <vgorin@mirantis.com>
2016-11-17 17:50:22 +0300

    Add new parameter to assign abs mount path

--- change_path.py
+++ change_path.py
@@ -5,20 +5,22 @@ import os
 def new_path(old_path):
     return old_path.replace('\\','/')
 
-def change_path(path):
+def change_path(path, mnt):
     new_p = new_path(path)
-    return os.path.abspath('apps_' + new_p[new_p.find('log'):])
+    return mnt +'/' + new_p[new_p.find('log'):]
 
 def cli():
     cli = argparse.ArgumentParser(prog="Programm summary",
                                 description="Description")
     cli.add_argument('-p', '--old_path', type=str,
                      help='Show new format path')
+    cli.add_argument('-m','--abs_mnt', type=str,
+                     help='Abs path to mount point of smb')
     return cli.parse_args()
 
 def main():
     args = cli()
-    print change_path(args.old_path) 
+    print change_path(args.old_path, args.abs_mnt) 
 
 if __name__ == '__main__':
     main()
